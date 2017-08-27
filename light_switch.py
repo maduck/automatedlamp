95ea35ee61f0cb04f7679ab21b6e277c9d298f3e
maduck <utti+github@uttisoft.de>
2017-08-27 14:42:40 +0200

    rearranged the script, refactored more

--- light_switch.py
+++ light_switch.py
@@ -4,24 +4,22 @@ import argparse
 import RPi.GPIO as GPIO
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('switch', choices=['on', 'off'], help='switch light on or off')
-parser.add_argument('pin', type=int, help='board pin to switch', required=True)
+parser.add_argument('--switch', choices=['on', 'off'], help='switch light on or off')
+parser.add_argument('--pin', type=int, help='board pin to switch', required=True)
 args = parser.parse_args()
 
 switch_choices = {
   'on': GPIO.HIGH,
   'off': GPIO.LOW,
 }
-switch = switch_choices[args.switch]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(args.pin, GPIO.OUT)
 
-if args.switch:
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setwarnings(False)
-  GPIO.setup(args.pin, GPIO.OUT) 
+if args.switch is not None:
+  switch = switch_choices[args.switch]
   GPIO.output(args.pin, switch)
 
-GPIO.setup(args.pin, GPIO.IN)
 status = GPIO.input(args.pin)
-print(status)
+print('on' if status else 'off')
 
 GPIO.cleanup()
